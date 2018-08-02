d2376873d18b5e8f1039142dde81ceadb2983efc
Julio Marquez <juliocmrqzm@gmail.com>
2018-08-02 14:58:12 -0400

    Create the markdown parser

---
+++ utils/markdown/containers.js
@@ -0,0 +1,36 @@
+const container = require('markdown-it-container')
+const isRFC3339 = require('validator/lib/isRFC3339')
+
+module.exports = md => {
+  md.use(...createUpdateContainer('update'))
+}
+
+function createUpdateContainer (klass) {
+  const parseInfo = (info) => info.match(/^update\s+(.*)\s+\|\s+(.*)$/)
+
+  return [container, klass, {
+    validate: function(params) {
+      const match = parseInfo(params.trim())
+
+      if (match !== null) {
+        // Make sure the date is in RFC3339 format
+        return isRFC3339(match[2])
+      } else {
+        return false
+      }
+    },
+    render (tokens, idx) {
+      const token = tokens[idx]
+
+      if (token.nesting === 1) {
+        const match = parseInfo(token.info.trim())
+        const title = match[1].trim()
+        const date = new Date(match[2]).toISOString()
+
+        return `<div class="update-block"><p class="update-block-title">${title}</p><p class="update-block-date">${date}</p>\n`
+      } else {
+        return `</div>\n`
+      }
+    }
+  }]
+}
---
+++ utils/markdown/index.js
@@ -0,0 +1,24 @@
+const emoji = require('markdown-it-emoji')
+
+const containers = require('./containers')
+const frontmatter = require('./frontmatter')
+
+
+module.exports = ({ markdown = {}} = {}) => {
+  const md = require('markdown-it')({
+    html: true
+  })
+  // custom plugins
+  .use(containers)
+  .use(frontmatter)
+
+  // 3rd party plugins
+  .use(emoji)
+
+  // apply user config
+  if (markdown.config) {
+    markdown.config(md)
+  }
+
+  return md
+}
