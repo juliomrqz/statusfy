80b18b7c5521fc2bd353bed6d48d9324b5591ea1
Julio Marquez <juliocmrqzm@gmail.com>
2018-08-12 15:46:12 -0400

    Fix the names of the API files

--- server/api/incidents.js
+++ server/api/incidents.js
@@ -1,10 +1,40 @@
 const { Router } = require('express')
 
-const incidents = require('./incidents')
+const createDatabase = require('../../lib/content/database')
+const response = require('../utils/response')
 
 const router = Router()
 
-// Add Incidents Routes
-router.use(incidents)
+router.get('/incidents', async (req, res, next) => {
+  const language = req.app.get('language')
+  const send = response(res, language)
+
+  try {
+    const database = await createDatabase(req.app.get('siteConfig'))
+
+    send.json(database.all(language))
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/incidents/:id', async (req, res, next) => {
+  const { id } = req.params
+  const language = req.app.get('language')
+  const send = response(res, language)
+
+  try {
+    const database = await createDatabase(req.app.get('siteConfig'))
+    const incident = database.get(id, language)
+
+    if (incident) {
+      send.json(database.get(id, language))
+    } else {
+      send.notFound('Incident not found.')
+    }
+  } catch (error) {
+    next(error)
+  }
+})
 
 module.exports = router
--- server/api/index.js
+++ server/api/index.js
@@ -1,40 +1,10 @@
 const { Router } = require('express')
 
-const createDatabase = require('../../lib/content/database')
-const response = require('../utils/response')
+const incidents = require('./incidents')
 
 const router = Router()
 
-router.get('/incidents', async (req, res, next) => {
-  const language = req.app.get('language')
-  const send = response(res, language)
-
-  try {
-    const database = await createDatabase(req.app.get('siteConfig'))
-
-    send.json(database.all(language))
-  } catch (error) {
-    next(error)
-  }
-})
-
-router.get('/incidents/:id', async (req, res, next) => {
-  const { id } = req.params
-  const language = req.app.get('language')
-  const send = response(res, language)
-
-  try {
-    const database = await createDatabase(req.app.get('siteConfig'))
-    const incident = database.get(id, language)
-
-    if (incident) {
-      send.json(database.get(id, language))
-    } else {
-      send.notFound('Incident not found.')
-    }
-  } catch (error) {
-    next(error)
-  }
-})
+// Add Incidents Routes
+router.use(incidents)
 
 module.exports = router
