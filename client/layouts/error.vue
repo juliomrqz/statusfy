c01d02633485e56f979dfe4b1393d3e32be7dbe2
Julio Marquez <juliocmrqzm@gmail.com>
2018-08-21 10:34:38 -0400

    Create the error page

---
+++ client/layouts/error.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="text-center py-6">
+    <div class="pt-6 h-64">
+      <div class="py-6">
+        <h1 class="text-5xl mb-2 font-medium">{{ title }}</h1>
+        <p class="text-xl my-4">{{ description }}</p>
+      </div>
+
+      <a
+        :href="localePath('index')"
+        class="font-medium mt-6">
+        {{ $t('error.go-back-link') }}
+      </a>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      required: true
+    }
+  },
+  head () {
+    return {
+      htmlAttrs: {
+        lang: this.$i18n.locale
+      },
+      title: this.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.description
+        }
+      ]
+    }
+  },
+  computed: {
+    title () {
+      const $t = this.$t.bind(this)
+
+      return this.error.statusCode === 404
+        ? $t('error.404.title')
+        : $t('error.other.title')
+    },
+    description () {
+      const $t = this.$t.bind(this)
+
+      return this.error.statusCode === 404
+        ? $t('error.404.description')
+        : $t('error.other.description')
+    }
+  }
+}
+</script>
