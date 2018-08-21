c31f30374e299b7b38fbdb243f127e8a12365e63
Julio Marquez <juliocmrqzm@gmail.com>
2018-08-21 17:35:09 -0400

    Remove the Statusfy version from the generator meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'generator', content: `Statusfy v${pkg.version}` },
+      { name: 'generator', content: 'Statusfy' }
     ]
   },
 
