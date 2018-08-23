820ef71f2642c9be0c425271e07c3e9f7898ebd5
Julio Marquez <juliocmrqzm@gmail.com>
2018-08-23 18:47:04 -0400

    Support Google Analytics

--- demo/config.js
+++ demo/config.js
@@ -2,6 +2,7 @@ module.exports = {
   title: 'Statusfy Demo',
   description: 'A marvelous open source Status Page system',
   baseUrl: 'https://demo.statusfy.co',
+  ga: process.env.GA_TRACKING_ID || 'UA-XXXXXXXXX-Y',
   defaultLocale: 'en',
   locales: [
     { code: 'en', iso: 'en-US', name: 'English', file: 'en.json' },
