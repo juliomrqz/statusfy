9a36430982b72ae984273b3395ea447d279c6b0e
Julio Marquez <juliocmrqzm@gmail.com>
2018-10-30 15:00:21 -0400

    chore(docs): create the ColorBox component

---
+++ packages/docs/src/.vuepress/components/ColorBox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :title="`Color: ${color}`">
+    <span class="box" :style="`background-color: ${color};`">&nbsp;</span>
+    <span class="text">{{ color }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .box {
+    border: 1px solid #e1e4e8;
+    height: 18px;
+    width: 18px;
+    display: inline-block;
+    border-radius: 4px;
+  }
+</style>
