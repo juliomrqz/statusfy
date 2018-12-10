28ed4eb1692406aea941857efa1063b9d8e1054c
Julio Marquez <juliocmrqzm@gmail.com>
2018-12-10 09:09:32 -0400

    fix(core): exclude the dynamic pages building from the SSR mode

--- packages/@statusfy/core/client/modules/statusfy/build.js
+++ packages/@statusfy/core/client/modules/statusfy/build.js
@@ -13,82 +13,84 @@ const asset = object => {
   }
 }
 
-module.exports = async function buildContent (nuxt, buildDir, isStatic, options) {
-  const database = await createDatabase(nuxt.options.statusfy.siteConfig)
-  const pathPrefix = '/content/api/v0'
+module.exports = async function buildContent (nuxt, isStatic) {
+  if (isStatic) {
+    const database = await createDatabase(nuxt.options.statusfy.siteConfig)
+    const pathPrefix = '/content/api/v0'
 
-  nuxt.options.build.plugins.push({
-    apply (compiler) {
-      compiler.plugin('emit', (compilation, cb) => {
-        nuxt.options.statusfy.locales.forEach(locale => {
-          // Systems
-          compilation.assets[`${pathPrefix}/systems.${locale.code}.json`] =
-            asset(database.systems(locale.code))
+    nuxt.options.build.plugins.push({
+      apply (compiler) {
+        compiler.plugin('emit', (compilation, cb) => {
+          nuxt.options.statusfy.locales.forEach(locale => {
+            // Systems
+            compilation.assets[`${pathPrefix}/systems.${locale.code}.json`] =
+              asset(database.systems(locale.code))
 
-          // Timeline
-          compilation.assets[`${pathPrefix}/incidents/timeline.${locale.code}.json`] =
-            asset(database.incidentsTimeline(locale.code))
+            // Timeline
+            compilation.assets[`${pathPrefix}/incidents/timeline.${locale.code}.json`] =
+              asset(database.incidentsTimeline(locale.code))
 
-          // All Incients Pages
-          const incidentsPage1 = database.incidents(locale.code)
-          let currentPage = 1
-          let totalPages = incidentsPage1.total_pages
+            // All Incients Pages
+            const incidentsPage1 = database.incidents(locale.code)
+            let currentPage = 1
+            let totalPages = incidentsPage1.total_pages
 
-          while (currentPage <= totalPages) {
-            const data = database.incidents(locale.code, currentPage)
+            while (currentPage <= totalPages) {
+              const data = database.incidents(locale.code, currentPage)
 
-            compilation.assets[`${pathPrefix}/incidents.page-${currentPage}.${locale.code}.json`] =
-              asset(data)
+              compilation.assets[`${pathPrefix}/incidents.page-${currentPage}.${locale.code}.json`] =
+                asset(data)
 
-            currentPage = data.page + 1
-          }
+              currentPage = data.page + 1
+            }
 
-          // History Pages
-          const historyPage1 = database.incidentsHistory(locale.code)
-          currentPage = 1
-          totalPages = historyPage1.total_pages
+            // History Pages
+            const historyPage1 = database.incidentsHistory(locale.code)
+            currentPage = 1
+            totalPages = historyPage1.total_pages
 
-          while (currentPage <= totalPages) {
-            const data = database.incidentsHistory(locale.code, currentPage)
+            while (currentPage <= totalPages) {
+              const data = database.incidentsHistory(locale.code, currentPage)
 
-            compilation.assets[`${pathPrefix}/incidents/history.page-${currentPage}.${locale.code}.json`] =
-              asset(data)
+              compilation.assets[`${pathPrefix}/incidents/history.page-${currentPage}.${locale.code}.json`] =
+                asset(data)
 
-            currentPage = data.page + 1
-          }
+              currentPage = data.page + 1
+            }
 
-          // Incident Detail Pages
-          const incidents = database.incidents(locale.code, 1, -1).incidents
+            // Incident Detail Pages
+            const incidents = database.incidents(locale.code, 1, -1).incidents
 
-          incidents.forEach(incident => {
-            compilation.assets[`${pathPrefix}/incidents/${incident.id}.${locale.code}.json`] =
-            asset(incident)
+            incidents.forEach(incident => {
+              compilation.assets[`${pathPrefix}/incidents/${incident.id}.${locale.code}.json`] =
+              asset(incident)
+            })
           })
+
+          cb()
         })
-
-        cb()
-      })
-    }
-  })
-
-  // Add Dynamic Pages
-  nuxt.options.generate.routes = []
-
-  nuxt.options.statusfy.locales.forEach(locale => {
-    const prefix = locale.code === nuxt.options.statusfy.siteConfig.defaultLocale ? '' : `/${locale.code}`
-
-    // History Pages
-    let totalPages = database.incidentsHistory(locale.code).total_pages
-
-    for (let i = 2; i <= totalPages; i++) {
-      nuxt.options.generate.routes.push(`${prefix}/history/${i}`)
-    }
-
-    // Incident Detail Pages
-    const incidents = database.incidents(locale.code, 1, -1).incidents
-
-    incidents.forEach(incident => {
-      nuxt.options.generate.routes.push(`${prefix}/incidents/${incident.id}`)
+      }
     })
-  })
+
+    // Add Dynamic Pages
+    nuxt.options.generate.routes = []
+
+    nuxt.options.statusfy.locales.forEach(locale => {
+      const prefix = locale.code === nuxt.options.statusfy.siteConfig.defaultLocale ? '' : `/${locale.code}`
+
+      // History Pages
+      let totalPages = database.incidentsHistory(locale.code).total_pages
+
+      for (let i = 2; i <= totalPages; i++) {
+        nuxt.options.generate.routes.push(`${prefix}/history/${i}`)
+      }
+
+      // Incident Detail Pages
+      const incidents = database.incidents(locale.code, 1, -1).incidents
+
+      incidents.forEach(incident => {
+        nuxt.options.generate.routes.push(`${prefix}/incidents/${incident.id}`)
+      })
+    })
+  }
 }
--- packages/@statusfy/core/client/modules/statusfy/index.js
+++ packages/@statusfy/core/client/modules/statusfy/index.js
@@ -82,7 +82,7 @@ module.exports = async function Statusfy () {
       })
     }
 
-    // Build dynamic content pages
+    // Build dynamic content
     await buildContent(this, isStatic)
   })
 
