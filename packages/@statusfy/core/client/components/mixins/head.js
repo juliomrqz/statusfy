fcffb6702a62039332f551fc5faeea6682fc7840
Julio Marquez <juliocmrqzm@gmail.com>
2018-12-11 14:27:32 -0400

    fix(core): add the Atom & RSS feeds alternate meta links

--- packages/@statusfy/core/client/components/mixins/head.js
+++ packages/@statusfy/core/client/components/mixins/head.js
@@ -1,5 +1,5 @@
 export default {
-  head () {
+  head() {
     const favicons = this.$statusfy.iconSizes.map(size => {
       return {
         hid: `favicon-${size}`,
@@ -11,11 +11,41 @@ export default {
 
     const canonical = `${this.$statusfy.baseUrl}${this.$route.path}`
 
+    // Atom and RSS
+    let linksFeeds = []
+    if (this.$statusfy.notifications && this.$statusfy.notifications.feeds) {
+      const feedsBaseUrl = this.$statusfy.baseUrl === '/'
+        ? '/feeds'
+        : `${this.$statusfy.baseUrl}/feeds`
+
+      linksFeeds = [{
+          rel: 'alternate',
+          type: 'application/rss+xml',
+          hid: 'alternate-feed-rss',
+          title: '',
+          href: `${feedsBaseUrl}/incidents.${this.$i18n.locale}.xml`
+        },
+        {
+          rel: 'alternate',
+          type: 'application/atom+xml',
+          hid: 'alternate-feed-atom',
+          title: '',
+          href: `${feedsBaseUrl}/incidents.${this.$i18n.locale}.atom`,
+        },
+      ]
+    }
+
     return {
       link: [
         ...favicons,
-        { rel: 'canonical', href: canonical, hid: 'canonical' }
-      ]
+        ...linksFeeds,
+        {
+          rel: 'canonical',
+          href: canonical,
+          hid: 'canonical'
+        },
+      ],
+
     }
   }
 }
